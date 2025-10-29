1201d6af1c7864590f435dfc1a27761a45698850
Kabbs19 <77408435+Kabbs19@users.noreply.github.com>
2025-10-29 17:37:09 +0000

    add getComputerChoice function

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+
 function getComputerChoice() {
-  
+
   let randomNumber = Math.random();
 
   if (randomNumber < 0.33) {
+    //"if" checks if something is true
     return "rock";
   } else if (randomNumber < 0.66) {
+    //"else if" checks if previous is false
     return "paper";
   } else {
+    //""else" checks if no other condition before is matched
     return "scissors";
+    //"return" gives value back from function
   }
 }
 
-
-console.log(getComputerChoice());
 
